85115cd219d6c09696a5edef422d72d4b7700fdc
Glen Currier <20134533+gcurrier@users.noreply.github.com>
2024-02-01 11:28:34 +0100

    added imports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.perpetuum.perpreqcalc {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
 
     opens com.perpetuum.perpreqcalc to javafx.fxml;
